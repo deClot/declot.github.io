cb73aedb74c4a887d086edee5accb9a812ae39d2
Nadezda Kashirina <refenement@gmail.com>
2017-07-20 23:25:25 +0000

    stylus and fonts

--- public/main.js
+++ public/main.js
@@ -109,14 +109,113 @@ if(false) {
 
 /***/ }),
 /* 2 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed: ParseError: expected \"indent\", got \"eos\"\n    at Parser.error (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:259:11)\n    at Parser.expect (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:287:12)\n    at Parser.block (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:845:12)\n    at Parser.selector (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:1555:24)\n    at Parser.ident (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:1446:25)\n    at Parser.stmt (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:778:26)\n    at Parser.statement (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:685:21)\n    at Parser.parse (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/parser.js:236:25)\n    at Renderer.render (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus/lib/renderer.js:80:22)\n    at /home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/stylus-loader/index.js:165:12\n    at tryCatchReject (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/when/lib/makePromise.js:840:30)\n    at runContinuation1 (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/when/lib/makePromise.js:799:4)\n    at Fulfilled.when (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/when/lib/makePromise.js:590:4)\n    at Pending.run (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/when/lib/makePromise.js:481:13)\n    at Scheduler._drain (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/when/lib/Scheduler.js:62:19)\n    at Scheduler.drain (/home/declo/Rizzoma/HTML_CSS/declot.github.io/node_modules/when/lib/Scheduler.js:27:9)\n    at _combinedTickCallback (internal/process/next_tick.js:95:7)\n    at process._tickCallback (internal/process/next_tick.js:161:9)");
+exports = module.exports = __webpack_require__(3)(undefined);
+// imports
+
+
+// module
+exports.push([module.i, "@font-face {\n  font-family: MyriadPro;\n  src: url(" + __webpack_require__(4) + ");\n  src: url(" + __webpack_require__(5) + ");\n}\n", ""]);
+
+// exports
+
 
 /***/ }),
-/* 3 */,
-/* 4 */,
-/* 5 */,
+/* 3 */
+/***/ (function(module, exports) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+module.exports = function(useSourceMap) {
+	var list = [];
+
+	// return the list of modules as css string
+	list.toString = function toString() {
+		return this.map(function (item) {
+			var content = cssWithMappingToString(item, useSourceMap);
+			if(item[2]) {
+				return "@media " + item[2] + "{" + content + "}";
+			} else {
+				return content;
+			}
+		}).join("");
+	};
+
+	// import a list of modules into the list
+	list.i = function(modules, mediaQuery) {
+		if(typeof modules === "string")
+			modules = [[null, modules, ""]];
+		var alreadyImportedModules = {};
+		for(var i = 0; i < this.length; i++) {
+			var id = this[i][0];
+			if(typeof id === "number")
+				alreadyImportedModules[id] = true;
+		}
+		for(i = 0; i < modules.length; i++) {
+			var item = modules[i];
+			// skip already imported module
+			// this implementation is not 100% perfect for weird media query combinations
+			//  when a module is imported multiple times with different media queries.
+			//  I hope this will never occur (Hey this way we have smaller bundles)
+			if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+				if(mediaQuery && !item[2]) {
+					item[2] = mediaQuery;
+				} else if(mediaQuery) {
+					item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+				}
+				list.push(item);
+			}
+		}
+	};
+	return list;
+};
+
+function cssWithMappingToString(item, useSourceMap) {
+	var content = item[1] || '';
+	var cssMapping = item[3];
+	if (!cssMapping) {
+		return content;
+	}
+
+	if (useSourceMap && typeof btoa === 'function') {
+		var sourceMapping = toComment(cssMapping);
+		var sourceURLs = cssMapping.sources.map(function (source) {
+			return '/*# sourceURL=' + cssMapping.sourceRoot + source + ' */'
+		});
+
+		return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
+	}
+
+	return [content].join('\n');
+}
+
+// Adapted from convert-source-map (MIT)
+function toComment(sourceMap) {
+	// eslint-disable-next-line no-undef
+	var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
+	var data = 'sourceMappingURL=data:application/json;charset=utf-8;base64,' + base64;
+
+	return '/*# ' + data + ' */';
+}
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "fonts/MyriadPro-Regular.woff";
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "fonts/MyriadPro-Regular.ttf";
+
+/***/ }),
 /* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
